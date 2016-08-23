159f06c90d79144b0405805e896fc3907e1a51a2
Gruve_p <gruve-p@users.noreply.github.com>
2016-08-23 17:23:52 +0200

    Fix IRC

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="electrum-grs-server",
     version="1.0",
     scripts=['run_electrum_grs_server.py','electrum-grs-server'],
-    install_requires=['plyvel','jsonrpclib', 'irc>=11', 'groestlcoin_hash'],
+    install_requires=['plyvel','jsonrpclib', 'irc==13.3.1', 'groestlcoin_hash'],
     dependency_links=['git+https://github.com/groestlcoin/groestlcoin-hash-python#egg=groestlcoin_hash'],
     package_dir={
         'electrumgrsserver':'src'
